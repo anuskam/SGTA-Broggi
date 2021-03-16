a1bc373369de104ab1fffe4d8895d46b9f3fcc89
DESKTOP-G8BOKOF\Alex <alxcanto@gmail.com>
2021-03-16 16:07:57 +0100

    Creating login page, basic template and adding css file, logo and favicon

---
+++ resources/views/login.blade.php
@@ -0,0 +1,52 @@
+@extends('template')
+
+@section('stylesheet')
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+@endsection
+
+@section('title', 'Autenticació')
+
+@section('content')
+    <div class="row" id="header">
+        <div class="col-12">
+            <img src="{{ asset('media/img/logo.png') }}" alt="" id="logo">
+        </div>
+    </div>
+    <div class="row" id="loginContainer">
+        <div class="card col-10 col-sm-6 col-md-6 col-lg-4 p-0" id="login-card">
+            <div class="card-header text-center" id="login-card-header">Inicia Sessió</div>
+            <div class="card-body" id="login-body">
+                <form enctype="multipart/form-data" action="" method="">
+                    <div class="form-group">
+                        <!-- User -->
+                        <div class="form-group row mt-2 ml-1">
+                            <div class="col-1 ml-1 mr-2 pt-1">
+                                <i class="fas fa-user fa-lg login-icon"></i>
+                            </div>
+                            <div class="col-9 ml-1 mr-1">
+                                <input class="form-control form-control-sm login-credentials" type="text" id="username" name="username"
+                                    placeholder="Usuari" autofocus>
+                            </div>
+                        </div>
+                        <!-- Password -->
+                        <div class="form-group row mt-4 ml-1">
+                            <div class="col-1 ml-1 mr-2 pt-1">
+                                <i class="fas fa-key fa-lg login-icon"></i>
+                            </div>
+                            <div class="col-9 ml-1 mr-1">
+                                <input class="form-control form-control-sm login-credentials" type="password" id="paswword" name="password"
+                                    minlength="8" placeholder="Contrasenya" autofocus required>
+                            </div>
+                        </div>
+                        <!-- Boto d'Entrar -->
+                        <div class="form-group row pt-4" id="loginButton">
+                            <div class="btn-group col-md-6">
+                                <input class="btn btn-block" type="submit" value="Accedir" name="login" id="login">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
