7d5d1a3e091543181649a35e3b6d4fe81be2c161
DESKTOP-G8BOKOF\Alex <alxcanto@gmail.com>
2021-03-18 12:19:49 +0100

    Adding role middleware and route group prototypes and kernel.php middleware definitions

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $roles)
+    {
+        if (!Auth::check()) {
+            $response = redirect('login');
+        }
+        else {
+            $user = Auth::user();
+            foreach ($roles as $role) {
+                if ($user->rol == $role){
+                    $response = $next($request);
+                }
+                else{
+                    $response = redirect('login');
+                }
+            }
+        }
+
+        return $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
     return view('login');
 });
+
+/* Routes that require the user to be logged in */
+Route::middleware(['auth'])->group(function () {
+
+});
+
+/* Routes that require the user to be admin or CECOS */
+Route::middleware(['role:1,2'])->group(function () {
+
+});
+
+/* Routes that require the user to be admin or Recurs */
+Route::middleware(['role:1,3'])->group(function () {
+
+});
+
+/* Routes that require the user to be admin */
+Route::middleware(['role:1'])->group(function () {
+
+});
+
