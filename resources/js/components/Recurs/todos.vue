ba600b0ff3430a76e88345d14ac368ec8771733d
anuskam <anuskam92@gmail.com>
2021-04-15 10:05:04 +0200

    todosRecursos modificado

--- resources/js/components/Recurs/todos.vue
+++ resources/js/components/Recurs/todos.vue
@@ -1,28 +1,27 @@
 <template>
 <main class="col-10">
-    <table class="table">
+  <table class="table mt-5">
     <thead>
       <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Sigles</th>
-        <th scope="col">Nom</th>
-        <th scope="col">Descripcio</th>
-        <th scope="col">Actiu</th>
-        <th scope="col"></th>
+        <th scope="col">Data</th>
+        <th scope="col">Municipi</th>
+        <th scope="col">Tipus d'incident</th>
+        <th scope="col">Tipus d'alertant</th>
+        <th scope="col">Telèfon</th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="cicle in cicles" :key="cicle.id">
-        <td>{{ cicle.id }}</td>
-        <td>{{ cicle.sigles }}</td>
-        <td>{{ cicle.nom }}</td>
-        <td>{{ cicle.descripcio }}</td>
+      <tr v-for="(incidencia, index) in incidencies" :key="incidencia.id">
+        <td>{{ incidencia.data }}</td>
         <td>
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" name="actiu[]" value="actiu" :checked="cicle.actiu" disabled>
-                <label for="actiu" class="custom-control-label"></label>
-            </div>
+          {{ getMunicipi(index) }}
         </td>
+        <td>
+          {{ getTipusIncidencia(index) }}
+        </td>
+        <td></td>
+        <td>{{ incidencia.telefon_alertant }}</td>
+        <td></td>
       </tr>
     </tbody>
 </table>
@@ -32,34 +31,83 @@
 
 <script>
     export default {
-        data(){
-            return{
-                cicles: [],
-                cicle:{
-                    id: '',
-                    sigles: '',
-                    nom: '',
-                    descripcio: '',
-                    actiu: false
-                },
-                insert : true
-            }
-        },
-        methods:{
-            selectCicles(){
-                let me = this;
-            axios.get('/SGTA-Broggi/public/api/cicle').then(response => {
-                me.cicles = response.data;
-            }).catch(error => {
-                console.log(error);
-            }).finally(() => this.loading = false)
-            }
-        },
-        created(){
-            this.selectCicles();
-        },
-        mounted() {
-            console.log('Component mounted.')
+      data(){
+        return{
+          incidencies: [],
+          incidencia: {
+            data: '',
+            municipis_id: null,
+            tipus_incidencies_id: null,
+            alertants_id: null,
+            telefon_alertant: ''
+          },
+          municipis: [],
+          alertants: [],
+          tipusIncidencies: [],
         }
+      },
+      methods:{
+        selectIncidencies() {
+          let me = this;
+          axios
+              .get('/SGTA-Broggi/public/api/incidencia')
+              .then(response => {
+                me.incidencies = resposne.data;
+              })
+              .catch(error => {
+                console.log(error);
+              })
+        },
+        selectMunicipis() {
+          let me = this;
+          axios
+              .get("/SGTA-Broggi/public/api/municipi")
+              .then((response) => {
+                me.municipis = response.data;
+              }).catch((error) => {
+                console.log(error);
+              })
+        },
+        getMunicipi(index) {
+          let municipi = this.municipis.find(obj => obj.id == this.incidencies[index].municipis_id);
+          let municipi_nom;
+          if (municipi != null){
+            municipi_nom = municipi.nom;
+          }
+          else{
+            municipi_nom = this.municipis.find(obj => obj.id == 1);
+          }
+
+          return municipi_nom;
+        },
+        selectTipusIncidencia() {
+          let me = this;
+          axios
+              .get("/SGTA-Broggi/public/api/tipusIncidencia")
+              .then((response) => {
+                me.tipusIncidencies = response.data;
+              }).catch((error) => {
+                console.log(error);
+              })
+        },
+        getTipusIncidencia(index) {
+          let tipusIncidencia = this.tipusIncidencies.find(obj => obj.id == this.incidencies[index].tipus_incidencies_id);
+          let tipusIncidencia_nom;
+          if (tipusIncidencia != null) {
+            tipusIncidencia_nom = tipusIncidencia.tipus;
+          }
+          else {
+            tipusIncidencia_nom = this.incidencies.find(obj => obj.id == 1);
+          }
+
+          return tipusIncidencia_nom;
+        }
+      },
+      created(){
+        this.selectIncidencies(), this.selectMunicipis();
+      },
+      mounted() {
+          console.log('Component mounted.')
+      }
     }
 </script>
